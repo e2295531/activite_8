f6a194fed7bf3b9c6501fcc6808ec12e2c1bb295
e2295531 <e2295531@cmaisonneuve.qc.ca>
2022-04-14 15:07:38 -0400

    création page index.html

---
+++ .vscode/activite8/script.js
@@ -0,0 +1,13 @@
+
+  
+  document.querySelector('#button').addEventListener('click', logWarning);
+  
+  function logWarning() {
+    console.log('Hello, Console!');
+    console.warn('attention');
+    console.error('erreur');
+    console.log('Message');
+
+  }
+  
+  
